24d6a9c9d694cbc4ac1f8fdf9d018fd197367abd
randallcw <randallcw@vcu.edu>
2018-09-20 11:17:22 -0400

    fixed? ...maybe?

--- Lab4.java
+++ Lab4.java
@@ -1,39 +1,48 @@
+import java.util.Arrays;
 public class Lab4 {
-	
+
 	public static void main(String[] args) {
 
-	int[] test1 = new int[1,2,3,4];
-	int[] ttest1 = new int[test1.whatever()];
-	
-	int[] test2 = new int[1,2,3];
-	int[] ttest2 = new int[test2.whatever()];
-	
-	int[] test3 = new int[1,2,3,4,5];
-	int[] ttest3 = new int[test3.whatever()];
-	
-	int[] test4 = new int[1,2];
-	int[] ttest4 = new int[test4.whatever()];
-	
-	int[] test5 = new int[1,2,3,4,5,6];
-	int[] ttest5 = new int[test5.whatever()];
-	
+		int[] test1 = {1,2,7,4};
+		whatever(test1);
 
+		int[] test2 = {1,9,3};
+		whatever(test2);
 
-public void whatever(int[] array1) {
-	int[] array2 = new array[array1.length()];
-	arrayIndex = 0;
-	
-	for (int i = array1.length() - 1; i >= 0 ; i--) {
-		array2[arrayIndex] = array1[i];
-		arrayIndex++;
+		int[] test3 = {7,2,3,4,5};
+		whatever(test3);
+
+		int[] test4 = {3,2};
+		whatever(test4);
+
+		int[] test5 = {1,2,7,4,5,6};
+		whatever(test5);
+
 	}
-	
-	for (int i = 0; i < array2.length() - 1; i++) {
-		System.out.print(array2[i]);
-		System.out.print(" ");
+
+	public static void whatever(int[] array1) {
+		int[] copy = new int[array1.length];
+		int[] array2 = new int[array1.length];
+		int arrayIndex = 0;
+		
+		for (int i = 0; i < array1.length; i++) {
+			copy[i] = array1[i];
+			
+		}
+		
+		Arrays.sort(copy);
+		
+		for (int i = array1.length - 1; i >= 0 ; i--) {
+			array2[arrayIndex] = copy[i];
+			arrayIndex++;
+		}
+
+		for (int i = 0; i < array2.length; i++) {
+			System.out.print(array2[i]);
+			System.out.print(" ");
+		}
+		System.out.println("");
+
 	}
-	System.out.println("");
-	
-}
-}
+
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
 Write a method that will sort an array.
+
+God, I hate this
+
+What is going wrong?
